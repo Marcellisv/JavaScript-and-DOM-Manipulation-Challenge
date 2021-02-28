8afbaeaedd3f21fb5d393748ce9830e166082c01
Marcellisv <marcellisv@gmail.com>
2021-02-28 16:56:22 -0500

    Completes app.js with working filters

--- static/js/app.js
+++ static/js/app.js
@@ -2,19 +2,73 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+//refrence the table body
 
-var tbody = d3.select("tbody");
-var button = d3.select("#filter-btn");
+var table_body = d3.select("tbody");
+
+
+var filter_button= d3.select('#filter-btn');
+
+ // clear table info before appending filtered data
+ function buildthetable(data) {
+  
+  table_body.html("");
+
+// create rows for data sighting
+  data.forEach((sighting) => {
+    
+    var row = table_body.append("tr");
+
+
+    Object.values(sighting).forEach((ufo) => {
+      var ufo_append_info = row.append("td");
+        ufo_append_info.text(ufo);
+      } 
+    );
+  });
+}
 
 //
-var date_data= d3.select(#datetime)
-var city_data = d3.select(#city)
-var state = d3.select(#reset-btn)
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+var filter_click = d3.select("#filter-btn");
 
-var populate = (datetime) =>
-        dataInput.foreach(ufosightins) =>
-                var row = tbody.append("tr");
-                columns.forEach(column => row.append(td).text(ufo_sightins[column]))
+filter_click.on("click", function() {
+  d3.event.preventDefault();
 
-var 
+  var date_html = d3.select("#datetime");
+  var filter_term = date_html.property("value");
+
+  var city_html= d3.select("#city")
+  var filter_term = city_html.property("value");
+
+
+  var state_html= d3.select("#state")
+  var filter_term = state_html.property("value");
+
+
+  var country_html= d3.select("#country")
+  var filter_term = country_html.property("value");
+
+
+  var shape_html= d3.select("#shape")
+  var filter_term = shape_html.property("value");
+
+  if (filter_term == "") {
+    buildthetable(tableData);
+    
+
+  } else {
+
+    var filer_response = tableData.filter(sighting => sighting.datetime === filter_term);
+    var filer_response = tableData.filter(sighting => sighting.city === filter_term);
+    var filer_response = tableData.filter(sighting => sighting.state === filter_term);
+    var filer_response = tableData.filter(sighting => sighting.country === filter_term);
+    var filer_response = tableData.filter(sighting => sighting.shape === filter_term);
+
+
+    buildthetable(filer_response);
+  }
+
+  });
+
+// build table
+buildthetable(tableData);
