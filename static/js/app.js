e4640b043da26ca5933825cd4c369262b68c3d28
Marcellisv <marcellisv@gmail.com>
2021-02-28 13:52:51 -0500

    working to complete app

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,19 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+
+//
+var date_data= d3.select(#datetime)
+var city_data = d3.select(#city)
+var state = d3.select(#reset-btn)
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+var populate = (datetime) =>
+        dataInput.foreach(ufosightins) =>
+                var row = tbody.append("tr");
+                columns.forEach(column => row.append(td).text(ufo_sightins[column]))
+
+var 
